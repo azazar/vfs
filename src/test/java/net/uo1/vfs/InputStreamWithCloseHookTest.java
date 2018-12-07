d28dcb2ac0437e30143c0a88825fe6b7cfb83a6d
Mikhail Yevchenko <m@uo1.net>
2018-12-07 13:26:51 +0300

    replace use of deprecated IOUtils.toString(String)

--- src/test/java/net/uo1/vfs/InputStreamWithCloseHookTest.java
+++ src/test/java/net/uo1/vfs/InputStreamWithCloseHookTest.java
@@ -6,6 +6,8 @@ package net.uo1.vfs;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 import org.apache.commons.io.IOUtils;
 
 /**
@@ -18,7 +20,7 @@ public class InputStreamWithCloseHookTest {
         StructuredFile f = new StructuredFile(new File("/tmp/1.zip"), "1");
         
         try (InputStream i = f.open()) {
-            System.out.println(IOUtils.toString(i));
+            System.out.println(IOUtils.toString(i, StandardCharsets.UTF_8));
         }
     }
     
